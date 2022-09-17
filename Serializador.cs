8d6d48145c00315c6f4995f01fc685f7dcd320c8
O-Javier-Hidalgo-R <66095323+O-Javier-Hidalgo-R@users.noreply.github.com>
2022-09-16 23:18:31 -0400

    Inicio del rep

---
+++ Serializador.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Proyecto1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloTriangle
+{
+    //Nuevo: clase que se usa para serializar y deserializar un objeto (triangulo) desde un .json
+    public class Serializador
+    {
+        //Atributo tipo string que lleva el directorio del archivo .json
+        private string _path;
+
+        //Construnctor por que permite nombrar el .json
+        public Serializador(string name)
+        {
+            _path = name + ".json";
+        }
+
+        //Metodo para serializar un objeto entrante tipo triangulo a archivo .json
+        public void Serializar(Triangulo triangulo)
+        {
+            //genera el string json
+            string json = JsonConvert.SerializeObject(triangulo, Formatting.Indented);
+
+            //guarda el string json como archivo .json
+            File.WriteAllText(_path, json);
+        }
+
+        //Funcion que retorna un triangulo con los valores obtenidos de un .json
+        public Triangulo Deserealizar()
+        {
+            string json;
+
+            //se obtiene en un string los valores guardados en el .json
+            using (var reader = new StreamReader(_path))
+            {
+                json = reader.ReadToEnd();
+            }
+
+            //agrega los valores del string json a un triangulo
+            Triangulo triangulo = JsonConvert.DeserializeObject<Triangulo>(json);
+
+            //retorna ese triangulo
+            return triangulo;
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using Proyecto1;
+
+namespace HelloTriangle
+{
+    public class Triangulo
+    {
+        public Punto centro;
+        public float ancho;
+        public float alto;
+
+        public Triangulo(Punto centro, float ancho, float alto)
+        {
+            this.centro = centro;
+            this.ancho = ancho;
+            this.alto = alto;
+        }   
+
+        public void dibujar()
+        {
+            PrimitiveType primitiveType = PrimitiveType.LineLoop;
+            vertices(primitiveType);
+        }
+
+        public void vertices(PrimitiveType primitive)
+        {
+            GL.Begin(primitive);
+            GL.Color4(Color.Red);
+
+            GL.Vertex3(centro.x, centro.y + (alto/2), centro.z);
+            GL.Vertex3(centro.x - (ancho / 2), centro.y - (alto / 2), centro.z);
+            GL.Vertex3(centro.x + (ancho / 2), centro.y - (alto / 2), centro.z);
+            GL.End();
+        }
+    }
+}
